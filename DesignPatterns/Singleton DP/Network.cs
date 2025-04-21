b99b3c9ff47e8fdf9f6f2cd273be579f2539c533
Shaban Hassan <shabanhassan1010@gmail.com>
2025-04-21 15:51:36 +0200

    Added Singleton DP

---
+++ DesignPatterns/Singleton DP/Network.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton_DP
+{
+    public class Network
+    {
+        // Now i have ref from Network and he refer to null
+        public static Network network;   
+        private Network()  
+        {
+            // Private Constructor
+        }
+        public static Network GetNetwork()
+        {
+            if(network == null)  
+            {
+                network = new Network(); // i will create Obj
+            }
+            return network;  // the object already created and i will reuse it
+        }
+    }
+}
